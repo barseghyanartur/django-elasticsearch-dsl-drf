44ea994acb5a2aeb26c997088a1244cc47a1517a
Artur Barseghyan <artur.barseghyan@gmail.com>
2017-09-22 00:03:10 +0200

    cleanup

--- src/django_elasticsearch_dsl_drf/filter_backends/filtering/geo_spatial.py
+++ src/django_elasticsearch_dsl_drf/filter_backends/filtering/geo_spatial.py
@@ -26,10 +26,7 @@ class GeoSpatialFilteringFilterBackend(BaseFilterBackend, FilterBackendMixin):
     Example:
 
         >>> from django_elasticsearch_dsl_drf.constants import (
-        >>>     LOOKUP_FILTER_PREFIX,
-        >>>     LOOKUP_FILTER_WILDCARD,
-        >>>     LOOKUP_QUERY_EXCLUDE,
-        >>>     LOOKUP_QUERY_ISNULL,
+        >>>     LOOKUP_FILTER_GEO_DISTANCE,
         >>> )
         >>> from django_elasticsearch_dsl_drf.filter_backends import (
         >>>     GeoSpatialFilteringFilterBackend

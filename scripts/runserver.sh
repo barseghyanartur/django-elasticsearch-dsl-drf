00dfa3bfdefd60f09dbfdd30491d2e5affcbef88
Artur Barseghyan <artur.barseghyan@gmail.com>
2018-07-06 10:31:58 +0200

    more

--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 cd examples/simple/
-./manage.py runserver 0.0.0.0:8000 --traceback -v 3 "$@"
+server="0.0.0.0"
+port="8000"
+if [ $1 == "--port" ]
+then
+    port="$2"
+    shift
+    shift
+    args="$@"
+else
+    port="8000"
+    args="$@"
+fi
+./manage.py runserver "$server:$port" --traceback -v 3 "$args"

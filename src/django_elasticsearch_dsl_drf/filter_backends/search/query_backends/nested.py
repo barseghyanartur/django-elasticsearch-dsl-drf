9e0ff730e3853d105f00630b65800768290e4e8d
Artur Barseghyan <artur.barseghyan@gmail.com>
2018-09-05 00:33:08 +0200

    changes in nested search; wip

--- src/django_elasticsearch_dsl_drf/filter_backends/search/query_backends/nested.py
+++ src/django_elasticsearch_dsl_drf/filter_backends/search/query_backends/nested.py
@@ -27,6 +27,51 @@ class NestedQueryBackend(BaseSearchQueryBackend):
         :param search_backend:
         :return:
         """
+        query_params = search_backend.get_search_query_params(request)
+        __queries = []
+        for search_term in query_params:
+            __values = search_backend.split_lookup_name(search_term, 1)
+            __len_values = len(__values)
+            if __len_values > 1:
+                field, value = __values
+                if field in view.search_fields:
+                    # Initial kwargs for the match query
+                    field_kwargs = {field: {'query': value}}
+                    # In case if we deal with structure 2
+                    if isinstance(view.search_nested_fields, dict):
+                        extra_field_kwargs = view.search_fields[field]
+                        if extra_field_kwargs:
+                            field_kwargs[field].update(extra_field_kwargs)
+                    # The match query
+                    __queries.append(
+                        Q(cls.query_type, **field_kwargs)
+                    )
+            else:
+                for field in view.search_nested_fields:
+                    # Initial kwargs for the match query
+                    field_kwargs = {field: {'query': search_term}}
+
+                    # In case if we deal with structure 2
+                    if isinstance(view.search_nested_fields, dict):
+                        extra_field_kwargs = view.search_fields[field]
+                        if extra_field_kwargs:
+                            field_kwargs[field].update(extra_field_kwargs)
+
+                    # The match query
+                    __queries.append(
+                        Q(cls.query_type, **field_kwargs)
+                    )
+        return __queries
+
+    @classmethod
+    def __construct_search(cls, request, view, search_backend):
+        """Construct search.
+
+        :param request:
+        :param view:
+        :param search_backend:
+        :return:
+        """
         if not hasattr(view, 'search_nested_fields'):
             return []
 

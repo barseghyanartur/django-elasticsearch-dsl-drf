f0e166e407a6f2cee8ae3cf9573d2087c6bcde72
Joseph Aditya Pandu Gunawan <josephpg_@outlook.com>
2019-06-26 22:18:25 +0700

    Fix Django REST Framework deprecations

--- examples/simple/search_indexes/urls.py
+++ examples/simple/search_indexes/urls.py
@@ -38,13 +38,13 @@ router = DefaultRouter()
 router.register(
     r'addresses',
     AddressDocumentViewSet,
-    base_name='addressdocument'
+    basename='addressdocument'
 )
 
 router.register(
     r'addresses-frontend',
     FrontAddressDocumentViewSet,
-    base_name='addressdocument_frontend'
+    basename='addressdocument_frontend'
 )
 
 # **********************************************************
@@ -53,7 +53,7 @@ router.register(
 router.register(
     r'authors',
     AuthorDocumentViewSet,
-    base_name='authordocument'
+    basename='authordocument'
 )
 
 # **********************************************************
@@ -62,109 +62,109 @@ router.register(
 router.register(
     r'books',
     BookDocumentViewSet,
-    base_name='bookdocument'
+    basename='bookdocument'
 )
 
 router.register(
     r'books-ordered-by-score',
     BookOrderingByScoreDocumentViewSet,
-    base_name='bookdocument_ordered_by_score'
+    basename='bookdocument_ordered_by_score'
 )
 
 router.register(
     r'books-functional-suggester',
     BookFunctionalSuggesterDocumentViewSet,
-    base_name='bookdocument_functional_suggester'
+    basename='bookdocument_functional_suggester'
 )
 
 router.register(
     r'books-frontend',
     BookFrontendDocumentViewSet,
-    base_name='bookdocument_frontend'
+    basename='bookdocument_frontend'
 )
 
 router.register(
     r'books-permissions',
     BookPermissionsDocumentViewSet,
-    base_name='bookdocument_permissions'
+    basename='bookdocument_permissions'
 )
 
 router.register(
     r'books-custom',
     BookCustomDocumentViewSet,
-    base_name='bookdocument_custom'
+    basename='bookdocument_custom'
 )
 
 router.register(
     r'books-ignore-index-errors',
     BookIgnoreIndexErrorsDocumentViewSet,
-    base_name='bookdocument_ignore_index_errors'
+    basename='bookdocument_ignore_index_errors'
 )
 
 router.register(
     r'books-more-like-this',
     BookMoreLikeThisDocumentViewSet,
-    base_name='bookdocument_more_like_this'
+    basename='bookdocument_more_like_this'
 )
 
 router.register(
     r'books-more-like-this-no-options',
     BookMoreLikeThisNoOptionsDocumentViewSet,
-    base_name='bookdocument_more_like_this_no_options'
+    basename='bookdocument_more_like_this_no_options'
 )
 
 router.register(
     r'books-default-filter-lookup',
     BookDefaultFilterLookupDocumentViewSet,
-    base_name='bookdocument_default_filter_lookup'
+    basename='bookdocument_default_filter_lookup'
 )
 
 router.register(
     r'books-compound-search-backend',
     BookCompoundSearchBackendDocumentViewSet,
-    base_name='bookdocument_compound_search_backend'
+    basename='bookdocument_compound_search_backend'
 )
 
 router.register(
     r'books-compound-search-boost-backend',
     BookCompoundSearchBoostSearchBackendDocumentViewSet,
-    base_name='bookdocument_compound_search_boost_backend'
+    basename='bookdocument_compound_search_boost_backend'
 )
 
 router.register(
     r'books-compound-search-backend-ordered-by-score',
     BookOrderingByScoreCompoundSearchBackendDocumentViewSet,
-    base_name='bookdocument_compound_search_backend_ordered_by_score'
+    basename='bookdocument_compound_search_backend_ordered_by_score'
 )
 
 router.register(
     r'books-source',
     BookSourceSearchBackendDocumentViewSet,
-    base_name='bookdocument_source'
+    basename='bookdocument_source'
 )
 
 router.register(
     r'books-multi-match-search-backend',
     BookMultiMatchSearchFilterBackendDocumentViewSet,
-    base_name='bookdocument_multi_match_search_backend'
+    basename='bookdocument_multi_match_search_backend'
 )
 
 router.register(
     r'books-multi-match-phrase-prefix-search-backend',
     BookMultiMatchOptionsPhasePrefixSearchFilterBackendDocumentViewSet,
-    base_name='bookdocument_multi_match_phrase_prefix_search_backend'
+    basename='bookdocument_multi_match_phrase_prefix_search_backend'
 )
 
 router.register(
     r'books-simple-query-string-search-backend',
     BookSimpleQueryStringSearchFilterBackendDocumentViewSet,
-    base_name='bookdocument_simple_query_string_search_backend'
+    basename='bookdocument_simple_query_string_search_backend'
 )
 
 router.register(
     r'books-simple-query-string-boost-search-backend',
     BookSimpleQueryStringBoostSearchFilterBackendDocumentViewSet,
-    base_name='bookdocument_simple_query_string_boost_search_backend'
+    basename='bookdocument_simple_query_string_boost_search_backend'
 )
 
 # **********************************************************
@@ -174,13 +174,13 @@ router.register(
 router.register(
     r'cities',
     CityDocumentViewSet,
-    base_name='citydocument'
+    basename='citydocument'
 )
 
 router.register(
     r'cities-compound-search-backend',
     CityCompoundSearchBackendDocumentViewSet,
-    base_name='citydocument_compound_search_backend'
+    basename='citydocument_compound_search_backend'
 )
 
 # **********************************************************
@@ -190,7 +190,7 @@ router.register(
 router.register(
     r'locations',
     LocationDocumentViewSet,
-    base_name='locationdocument'
+    basename='locationdocument'
 )
 
 # **********************************************************
@@ -200,7 +200,7 @@ router.register(
 router.register(
     r'publishers',
     PublisherDocumentViewSet,
-    base_name='publisherdocument'
+    basename='publisherdocument'
 )
 
 # **********************************************************

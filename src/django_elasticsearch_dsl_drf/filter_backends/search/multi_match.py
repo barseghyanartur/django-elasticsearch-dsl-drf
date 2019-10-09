6c64567c7d0e4694d706a554f1207f6b5021c7fe
Artur Barseghyan <artur.barseghyan@gmail.com>
2019-10-09 22:59:36 +0200

    mpre

--- src/django_elasticsearch_dsl_drf/filter_backends/search/multi_match.py
+++ src/django_elasticsearch_dsl_drf/filter_backends/search/multi_match.py
@@ -1,9 +1,7 @@
 """Multi match search filter backend."""
 import logging
 
-from django.core.exceptions import ImproperlyConfigured
-
-from ...constants import MATCHING_OPTION_MUST, MATCHING_OPTIONS
+from ...constants import MATCHING_OPTION_MUST
 from .base import BaseSearchFilterBackend
 from .query_backends import (
     MultiMatchQueryBackend,
@@ -31,48 +29,3 @@ class MultiMatchSearchFilterBackend(BaseSearchFilterBackend):
     query_backends = [
         MultiMatchQueryBackend,
     ]
-
-    # def filter_queryset(self, request, queryset, view):
-    #     """Filter the queryset.
-    #
-    #     :param request: Django REST framework request.
-    #     :param queryset: Base queryset.
-    #     :param view: View.
-    #     :type request: rest_framework.request.Request
-    #     :type queryset: elasticsearch_dsl.search.Search
-    #     :type view: rest_framework.viewsets.ReadOnlyModelViewSet
-    #     :return: Updated queryset.
-    #     :rtype: elasticsearch_dsl.search.Search
-    #     """
-    #     if self.matching not in MATCHING_OPTIONS:
-    #         raise ImproperlyConfigured(
-    #             "Your `matching` value does not match the allowed matching"
-    #             "options: {}".format(', '.join(MATCHING_OPTIONS))
-    #         )
-    #
-    #     __queries = []
-    #
-    #     for query_backend in self._get_query_backends(request, view):
-    #         __queries.extend(
-    #             query_backend.construct_search(
-    #                 request=request,
-    #                 view=view,
-    #                 search_backend=self
-    #             )
-    #         )
-    #
-    #     if __queries:
-    #         # LOGGER.debug(
-    #         #     six.moves.reduce(operator.or_, __queries).to_dict()
-    #         # )
-    #
-    #         # Multiple multi match queries are not supported. We pick the
-    #         # first one only.
-    #         queryset = queryset.query(__queries[0])
-    #         # queryset = queryset.query(
-    #         #     query=six.moves.reduce(operator.or_, __queries)
-    #         # )
-    #
-    #     # LOGGER.debug(queryset.to_dict())
-    #
-    #     return queryset

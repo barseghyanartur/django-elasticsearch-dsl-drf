26009880a7d902e554b151d8df743512829b7e18
Artur Barseghyan <artur.barseghyan@gmail.com>
2021-04-28 23:36:47 +0200

    Improve

--- src/django_elasticsearch_dsl_drf/__init__.py
+++ src/django_elasticsearch_dsl_drf/__init__.py
@@ -7,9 +7,10 @@ __version__ = '0.22'
 __author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
 __copyright__ = '2017-2020 Artur Barseghyan'
 __license__ = 'GPL 2.0/LGPL 2.1'
-__all__ = ('default_app_config',)
 
-import django
 
-if django.VERSION < (3, 2):
+from django_nine import VERSIONS
+
+if VERSIONS.DJANGO_LT_3_2:
+    __all__ = ('default_app_config',)
     default_app_config = 'django_elasticsearch_dsl_drf.apps.Config'

2f1b4574ff25c806a284ee96f47ff3b56e58303a
Artur Barseghyan <artur.barseghyan@gmail.com>
2022-11-21 15:01:22 +0100

    Allow custom field name in HighlightBackend
    Allow custom field name in `HighlightBackend`.
    
    ```python
        highlight_fields = {
            "body": {"enabled": True, "options": {"fragment_size": 50, "number_of_fragments": 1_000}},
            "name": {"field": "name.text", "enabled": True, "options": {"fragment_size": 50, "number_of_fragments": 1_000}},
        }
    ```

--- src/django_elasticsearch_dsl_drf/filter_backends/highlight.py
+++ src/django_elasticsearch_dsl_drf/filter_backends/highlight.py
@@ -124,6 +124,7 @@ class HighlightBackend(BaseFilterBackend):
         highlight_fields = self.prepare_highlight_fields(view)
         for __field, __options in highlight_fields.items():
             if __field in highlight_query_params or __options['enabled']:
-                queryset = queryset.highlight(__field, **__options['options'])
+                __field_name = __options.get("field", __field_name)
+                queryset = queryset.highlight(__field_name, **__options['options'])
 
         return queryset
